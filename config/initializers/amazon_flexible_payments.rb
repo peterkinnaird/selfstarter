cd1662595c61e9c690fabe0c38bb4df44cfead84
Peter Kinnaird <peter.kinnaird@gmail.com>
2013-08-05 23:05:13 -0700

    swapped live version into sandbox mode for demo

--- config/initializers/amazon_flexible_payments.rb
+++ config/initializers/amazon_flexible_payments.rb
@@ -1,3 +1,3 @@
 AmazonFlexPay.access_key = ENV['AMAZON_ACCESS_KEY']
 AmazonFlexPay.secret_key = ENV['AMAZON_SECRET_KEY']
-AmazonFlexPay.go_live! if Rails.env.production?
+#AmazonFlexPay.go_live! if Rails.env.production?
